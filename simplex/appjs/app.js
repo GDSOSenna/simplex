6e9f0f08d645c2076776d6d66d5ca4b41d42d070
GDSOSenna <gustavosenna09@hotmail.com>
2023-05-23 18:37:44 -0300

    second commit

---
+++ simplex/appjs/app.js
@@ -0,0 +1,16 @@
+let v;
+let r;
+
+function gerar() {
+  v = document.getElementById('variable').value
+  r = document.getElementById('r').value
+
+  console.log(v, r)
+}
+
+function restri(){
+
+}
+
+
+
