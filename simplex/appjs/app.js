cadecf00c6ede6ed478cb6e18c13bbbd73d6def3
GDSOSenna <gustavosenna09@hotmail.com>
2023-05-25 22:29:25 -0300

    :boom: fix: Retirando página1 e alterando código ineficiente

--- simplex/appjs/app.js
+++ simplex/appjs/app.js
@@ -1,16 +1,21 @@
-let v;
-let r;
+/*Entrada 
+Inserir quantas variáveis de decisão possui o problema
+Inserir número de restrições
 
-function gerar() {
-  v = document.getElementById('variable').value
-  r = document.getElementById('r').value
+Objetivo
+Criar as restrições baseada na quantidade indicada pelo usuário
+Criar o número de variáveis somando a quantidade de variáveis de decisão com o número de restrições (trabalhando elas em forma de array)
 
+Saída
+Tabela com variáveis
+Solução ótima do problema de otimização linear
+*/
+
+let v= new Array()
+let r= new Array()
+
+function gerar(variaveis, restricoes) {
+  v.push(variaveis)
+  r.push(restricoes)  
   console.log(v, r)
 }
-
-function restri(){
-
-}
-
-
-
